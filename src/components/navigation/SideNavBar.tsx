844fec0adfc61cbc4cb671115a61f9dad8e9363f
MarcusQuixabeira <mquixaba@gmail.com>
2025-05-13 15:36:27 -0300

    Adjusting active nav links logic

--- src/components/navigation/SideNavBar.tsx
+++ src/components/navigation/SideNavBar.tsx
@@ -4,20 +4,28 @@ function SideNavBar() {
   return (
     <>
       <aside className="h-full w-(--sidebar-width) bg-zinc-300">
-        <nav className="mt-10">
-          <ul className="flex-column">
-            <NavLink
-              to="/languages"
-            >
-              <li className="p-4 hover:bg-zinc-500 hover:text-white hover:border-l-4 hover:cursor-pointer border-green-400">
+        <nav className="mt-10 h-full">
+          <ul className="flex flex-col h-full">
+            <li className="flex">
+              <NavLink
+                to="/languages"
+                className={({ isActive }) =>
+                  isActive ? "p-4 w-full bg-zinc-500 text-white border-l-4 cursor-pointer border-green-400" : "p-4 w-full hover:bg-zinc-500 hover:text-white hover:border-l-4 hover:cursor-pointer border-zinc-400"
+                }
+              >
                 Languages
-              </li>
-            </NavLink>
-            <NavLink to="/headers">
-              <li className="p-4 hover:bg-zinc-500 hover:text-white hover:border-l-4 hover:cursor-pointer border-green-400">
+              </NavLink>
+            </li>
+            <li className="flex">
+              <NavLink
+                to="/headers"
+                className={({ isActive }) =>
+                  isActive ? "p-4 w-full bg-zinc-500 text-white border-l-4 cursor-pointer border-green-400" : "p-4 w-full hover:bg-zinc-500 hover:text-white hover:border-l-4 hover:cursor-pointer border-zinc-400"
+                }
+              >
                 Headers
-              </li>
-            </NavLink>
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </aside>
